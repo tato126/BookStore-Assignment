85aac0b10b8f2efcd9f8050eecd3c40c4d93142a
tato126 <heechan.log@gmail.com>
2025-09-13 16:47:57 +0900

    initial commit

---
+++ src/test/java/org/bookstore/bookstore/BookStoreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bookstore.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
